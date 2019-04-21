bb7853dea51d959163a6acfda3c2b95b72785b4f
Naseem Ali <naseem.ali.925@gmail.com>
2019-04-21 22:29:07 +0530

    Home Fragment Changes

--- app/src/main/java/ali/naseem/unmatched/fragments/HomeFragment.java
+++ app/src/main/java/ali/naseem/unmatched/fragments/HomeFragment.java
@@ -3,11 +3,15 @@ package ali.naseem.unmatched.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import ali.naseem.unmatched.R;
+import ali.naseem.unmatched.adapters.SubjectsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,7 +37,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        TextView warning=view.findViewById(R.id.warning);
+        warning.setText(Html.fromHtml("Your fre trail period has expired on <font color=\"#448AFF\">29 Jan 2019</font> please upgrade now to get full access."));
+        RecyclerView recyclerView=view.findViewById(R.id.recyclerView);
+        SubjectsAdapter subjectsAdapter=new SubjectsAdapter();
+        return view;
     }
 
 }
